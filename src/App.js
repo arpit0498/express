98b1ceec4d26a39a71e233d49cf9bcd67af98eb7
arpit gupta <mycontact.arpit@gmail.com>
2023-06-29 23:47:08 +0530

    backend

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import { Navbar, RequireAuth } from "components";
+import { Bookmark, Explore, Home, Login, Profile, Signup } from "features";
+import { getAllPosts } from "features/post/postSlice";
+import { getAllUsers, getBookmarks } from "features/profile/userSlice";
+import MockmanEs from "mockman-js";
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { Route, Routes } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import "./App.css";
 
 function App() {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(getBookmarks())
+  }, [dispatch])
+
+  useEffect(() => {
+    dispatch(getAllUsers())
+    dispatch(getAllPosts())
+  }, [dispatch])
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar />
+      <ToastContainer
+        position="top-right"
+        autoClose={1000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick />
+
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route element={<RequireAuth />}>
+          <Route path="/home" element={<Home />} />
+          <Route path="/explore" element={<Explore />} />
+          <Route path="/bookmarks" element={<Bookmark />} />
+          <Route path="/profile/:currUsername" element={<Profile />} />
+        </Route>
+        <Route path="/mock" element={<MockmanEs />} />
+      </Routes>
     </div>
   );
 }
